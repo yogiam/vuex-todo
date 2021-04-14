27c1ecd12e2143f6207644c3b6a3810b09ddbfb2
yogesh.m <yogesh.mujumdar@viraltech.in>
2021-04-14 23:17:05 +0530

    basic todo fetch functionality added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="app">
+    <div class="container">
+      <todos/>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Todos from '@/components/Todos.vue'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  components: { Todos },
+  name: "app",
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+</script>
+<style scoped>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import store from './store' //imports index.js by default
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
