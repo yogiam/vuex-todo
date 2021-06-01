b215cda844b31a0d0d2db8411325bb1024e3a440
yogesh.m <yogesh.mujumdar@viraltech.in>
2021-06-01 22:55:06 +0530

    added firestore configuration to use it as a todos store

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAR1Q-0KRnPtbrXXJ9sksmsRA6fO9twsHw",
+    authDomain: "vuex-firebase-todo-manager.firebaseapp.com",
+    projectId: "vuex-firebase-todo-manager",
+    storageBucket: "vuex-firebase-todo-manager.appspot.com",
+    messagingSenderId: "1057110639692",
+    appId: "1:1057110639692:web:1be5fe2a99a9ff810420e9"
+}
